a541893a77e5a2017fc6edcc614cb8436df8083f
Joe Gencarelli <jp.genc@gmail.com>
2022-05-16 13:28:09 -0400

    Added all JS functionalities to external JS file

--- script.js
+++ script.js
@@ -1,7 +1,70 @@
-// Javascript for comments <form> section on the Blog Page
 
 
+// SLIDE Out Menu
 
+// Query the hamburger menu button
+const menuButton=document.querySelector('.menu-button');
+
+// Query the exit button in the slide out nav
+const exitButton=document.querySelector('.exit-button');
+        
+// Add an event listener to the hamburger menu button
+menuButton.addEventListener('click', ()=>{
+        
+    // Query the slide out menu div
+    const slideMenu=document.querySelector('.slide-out-menu');
+    
+    // Add a transition to the menu item and change it's width
+    slideMenu.style.transition="width .3s ease-in";
+    slideMenu.style.width="50vw";
+
+        // Add an event listener to the exit button to close the menu
+        exitButton.addEventListener('click', ()=>{
+        
+        // Set width of slide-out to 0
+            slideMenu.style.width="0";
+            });
+    });
+
+
+  // IMAGE CAROUSAL-------------------------------------------------------------------
+
+// Decalre an array with image sources
+const imgArray = ["./assets/gallery-image-1.jpg","./assets/gallery-image-2.jpg","./assets/gallery-image-3.jpg"];
+        
+// Query the image buttomn
+const buttonImg=document.querySelector(".image-gallery button");
+// decalre a count =0 
+let x=0;
+
+// Add an event Listener to the buttton
+buttonImg.addEventListener('click', (event)=>{
+// Prevent the default
+    event.preventDefault();
+    const gallery=document.querySelector(".image-gallery ul");
+    // Check if x is less than the array length
+    if(x<imgArray.length){
+        console.log(x);
+        // the imageSrc varibale will hold the source based on x - the position in the arrat
+        const imageSrc=imgArray[x];
+        // Query the image gallery ul
+        // const gallery=document.querySelector(".image-gallery ul");
+        // Set the innerHTML to that pf that img at source X
+        gallery.innerHTML=(`<li><img src=${imageSrc}></li>`);
+
+        // Add one to the count
+        x+=1;
+    }
+    else {
+        imageSrc=imgArray[0];
+        gallery.innerHTML=(`<li><img src=${imageSrc}></li>`);
+        x=0;
+        console.log(x);
+    }
+            
+});
+
+//COMMNENTS SECTION ON BLOG PAGE------------------------------------------------------
 // Query Select the form in the comments section
 const formInput = document.querySelector(".comments-container form");
 
